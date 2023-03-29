ae790241a7174d2520e0a9a027b0de4c0a25ed37
吕卓 <772243930@qq.com>
2023-03-29 12:50:06 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  title: '磁吸动力',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+   plugins: ['one-click-copy', {
+    copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+    copyMessage: 'Copied successfully!', // default is 'Copied successfully!'
+    toolTipMessage: 'Copy to clipboard', // default is ''Copy to clipboard'
+    duration: 300, // prompt message display time
+  }],
+  themeConfig: {
+    smoothScroll: true,
+    lastUpdated: 'Last Updated', //最后更新时间
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '磁吸', link: '/cx' },
+      { text: '文档', link: '/guide/' },
+      { text: '软件下载', link: '/download' },
+      { text: 'GitHub', link: 'https://github.com' },
+    ],
+    sidebar: [
+       {
+        title: '指南',
+        collapsable: false,
+        children: [
+          '/guide/introduction.md'
+        ]
+      },
+      {
+        title: '操作系统',
+        collapsable: true,
+        children: [
+          '/guide/mac.md',
+          '/guide/centos.md',
+          '/guide/ubuntu.md'
+        ]
+      },
+      {
+        title: 'linux命令',
+        collapsable: true,
+        children: [
+          '/guide/mv.md',
+          '/guide/ls.md',
+          '/guide/cp.md'
+        ]
+      }
+    ]
+  }
+}
+
